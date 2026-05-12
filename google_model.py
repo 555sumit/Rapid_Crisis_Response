916b49f625dbba1eefbd544ae69f5539f1027685
Devanshu2005-ally <devanshuverma345@gmail.com>
2026-05-12 11:37:25 +0530

    add system message

--- google_model.py
+++ google_model.py
@@ -1,6 +1,9 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import SystemMessage, HumanMessage
 from dotenv import load_dotenv
+import json
+import base64
+
 
 load_dotenv()
 
@@ -8,17 +11,46 @@ llm = ChatGoogleGenerativeAI(model="models/gemini-2.5-flash",
   temperature=0.2,
   max_tokens=2048)
 
-system_message = SystemMessage(content="""You are a helpful assistant. Keep responses concise. 
-do not include any formatting like adding stars keep answers in plain text. """)
+system_message = SystemMessage(content="""you are an emergency response assistant. Your task is to
+ analyze the given emergency situation(text, image, or both) and provide structured information 
+ that can help emergency responders assess the situation and determine the necessary actions. The
+  information you provide should be concise, accurate, and relevant to the emergency at hand.
+  return the information with the following fields:
+  "category":"",
+  "severity":"",
+  "location":"",
+  "people_involved": number of people involved
+  "services_required":[],
+  "summary":"",
+  "confidence_score": number between 0 and 1 representing the confidence level of the information provided
+  
+  for external services required include only required or emergency services.
+ 
 
-while True:
-    user_input = input("User: ")
-    if user_input.lower() in ['exit', 'quit']:
-        print("Exiting the chat. Goodbye!")
-        break
-    else:
-        human_message = HumanMessage(content=user_input)
-        output = llm.invoke([system_message, human_message])
-        print("Assistant:", output.content)
+  if the user query is not about the emergency analysis, respond normally in plain text form
 
-    
+  
+  
+  """)
+
+text = '''tell me something about yourself'''
+
+with open("burning-shopping-center-mall-with-smoke.jpg", "rb") as f:
+    image_data = base64.b64encode(f.read()).decode("utf-8")
+
+message = HumanMessage(
+    content=[
+        # {
+        #     "type": "text",
+        #     "text": text
+        # }
+        {
+            "type": "image_url",
+            "image_url": f"data:image/jpeg;base64,{image_data}"
+        }
+    ]
+)
+
+response = llm.invoke([system_message, message])
+
+print(response.content)
